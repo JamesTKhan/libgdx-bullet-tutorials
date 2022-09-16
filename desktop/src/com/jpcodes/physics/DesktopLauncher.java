a44315675f54a7be51a2f404a270de4d7de0d4f9
James <wjamesfl@gmail.com>
2022-09-15 22:46:37 -0400

    initial commit

---
+++ desktop/src/com/jpcodes/physics/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.jpcodes.physics;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("libGDX Bullet Tutorials");
+		new Lwjgl3Application(new BulletPhysics(), config);
+	}
+}
