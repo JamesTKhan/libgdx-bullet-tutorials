a7027be3f84d0db8807411de64f4057428d2146d
James <wjamesfl@gmail.com>
2022-10-02 18:49:07 -0400

    Add raycasting code

--- core/src/com/jpcodes/physics/BulletPhysicsSystem.java
+++ core/src/com/jpcodes/physics/BulletPhysicsSystem.java
@@ -1,7 +1,10 @@
 package com.jpcodes.physics;
 
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.DebugDrawer;
+import com.badlogic.gdx.physics.bullet.collision.ClosestRayResultCallback;
+import com.badlogic.gdx.physics.bullet.collision.RayResultCallback;
 import com.badlogic.gdx.physics.bullet.collision.btBroadphaseInterface;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionConfiguration;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionDispatcher;
@@ -52,6 +55,11 @@ public class BulletPhysicsSystem implements Disposable {
 
     private final float fixedTimeStep = 1/60f;
 
+    // Debug drawing ray casts
+    private final Vector3 lastRayFrom = new Vector3();
+    private final Vector3 lastRayTo = new Vector3();
+    private final Vector3 rayColor = new Vector3(1, 0, 1);
+
     public BulletPhysicsSystem() {
         collisionConfig = new btDefaultCollisionConfiguration();
         dispatcher = new btCollisionDispatcher(collisionConfig);
@@ -67,19 +75,53 @@ public class BulletPhysicsSystem implements Disposable {
         dynamicsWorld.setDebugDrawer(debugDrawer);
     }
 
+    /**
+     * Update physics world, should be called every frame
+     * @param delta deltaTime since last frame
+     */
     public void update(float delta) {
         // performs collision detection and physics simulation
-        dynamicsWorld.stepSimulation(delta, 1, fixedTimeStep);
+        dynamicsWorld.stepSimulation(delta, 5, fixedTimeStep);
     }
 
+    /**
+     * Debug draw the physics world
+     * @param camera camera to render to
+     */
     public void render(Camera camera) {
         debugDrawer.begin(camera);
+        debugDrawer.drawLine(lastRayFrom, lastRayTo, rayColor);
         dynamicsWorld.debugDrawWorld();
         debugDrawer.end();
     }
 
+    /**
+     * Add a rigid body to the physics world
+     * @param body the body to add
+     */
     public void addBody(btRigidBody body) {
         dynamicsWorld.addRigidBody(body);
+    }
+
+    /**
+     * Perform a raycast in the physics world.
+     * @param from the starting position (origin) of the ray
+     * @param to the end position of the ray
+     * @param callback the callback object to use
+     */
+    public void raycast(Vector3 from, Vector3 to, RayResultCallback callback) {
+        lastRayFrom.set(from).sub(0, 5f, 0f);
+
+        dynamicsWorld.rayTest(from, to, callback);
+
+        if (callback.hasHit() && callback instanceof ClosestRayResultCallback) {
+            // Use interpolation to determine the hitpoint where the ray hit the object
+            // This is what bullet does behind the scenes as well
+            lastRayTo.set(from);
+            lastRayTo.lerp(to, callback.getClosestHitFraction());
+        } else {
+            lastRayTo.set(to);
+        }
     }
 
     @Override
--- core/src/com/jpcodes/physics/screens/RigidBodyPhysics.java
+++ core/src/com/jpcodes/physics/screens/RigidBodyPhysics.java
@@ -1,6 +1,8 @@
 package com.jpcodes.physics.screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Material;
@@ -15,7 +17,10 @@ import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.SphereShapeBuilder;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
+import com.badlogic.gdx.physics.bullet.collision.ClosestRayResultCallback;
 import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
 import com.badlogic.gdx.physics.bullet.collision.btConeShape;
 import com.badlogic.gdx.physics.bullet.collision.btCylinderShape;
@@ -28,12 +33,45 @@ import com.jpcodes.physics.MotionState;
  * @version September 29, 2022
  */
 public class RigidBodyPhysics extends BaseScreen {
+    private static final Vector3 rayFromWorld = new Vector3();
+    private static final Vector3 rayToWorld   = new Vector3();
+    private final ClosestRayResultCallback callback = new ClosestRayResultCallback(new Vector3(), new Vector3());
 
     public RigidBodyPhysics(Game game) {
         super(game);
 
         createFloor(20, 1, 20);
         createObjects();
+    }
+
+    @Override
+    public void render(float delta) {
+        // On left click perform raycast
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            // Reset the callback
+            callback.setClosestHitFraction(1.0f);
+            callback.setCollisionObject(null);
+
+            // Get a pick ray of the current mouse coordinates
+            Ray ray = camera.getPickRay(Gdx.input.getX(), Gdx.input.getY());
+
+            rayFromWorld.set(ray.origin);
+            rayToWorld.set(ray.direction).scl(100f).add(ray.origin);
+
+            bulletPhysicsSystem.raycast(rayFromWorld, rayToWorld, callback);
+
+            if (callback.hasHit()) {
+                btCollisionObject collisionObject = callback.getCollisionObject();
+                if (collisionObject instanceof btRigidBody) {
+                    // Activate and push the object in the direction of the ray
+                    collisionObject.activate();
+                    ((btRigidBody) collisionObject).applyCentralImpulse(ray.direction.scl(50f));
+                }
+            }
+        }
+
+
+        super.render(delta);
     }
 
     private void createObjects() {
